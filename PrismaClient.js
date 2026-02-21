671103741dead08fb7f31b4b8893d62732b1dd5d
Kesia Pimenta <kesiapimentaferreira525@gmail.com>
2026-02-21 14:20:57 -0300

    correção em arquivos para rodar a aplicação

--- PrismaClient.js
+++ PrismaClient.js
@@ -1,10 +1,10 @@
+import { PrismaPg } from '@prisma/adapter-pg'
+import { PrismaClient } from '@prisma/client' 
 
+const adapter = new PrismaPg({ 
+    connectionString: process.env.DATABASE_URL
+ })
 
-
-
-
-import { PrismaClient } from '@prisma/client';
-
-const prisma = new PrismaClient();
+const prisma = new PrismaClient({ adapter, log: ["query"]});
 
 export default prisma;
